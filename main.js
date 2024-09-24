e8864aab0e15ea8f6977fc99ca7679886ff71ce1
Mass-hideo <massimo.viviano09@myhunter.cuny.edu>
2024-09-24 10:30:47 -0400

    Start page, loading & CSS
    Added Opening HTML and CSS
    WIll implement GSAP Next

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+document.addEventListener("DOMContentLoaded", (event) =>{
+    const container = document.querySelector('.loadpage');
 
+    for(let i = 0; i < 100; i++){
+        const stars = document.createElement('span');
+        stars.innerHTML = '✨';
+        stars.style.top = getRndInteger(0, container.clientHeight) + 'px';
+        stars.style.left = getRndInteger(0, container.clientWidth) + 'px';
+        container.appendChild(stars);
+    }
+});
+
+function getRndInteger(min, max){
+    return Math.floor(Math.random() * (max - min)) + min;
+};
