11bcd1e8776cd6db8257bf5116c8a426daaec07b
Mass-hideo <massimo.viviano09@myhunter.cuny.edu>
2024-09-26 11:30:42 -0400

    made responsive, added again button

--- main.js
+++ main.js
@@ -15,10 +15,22 @@ function getRndInteger(min, max){
     return Math.floor(Math.random() * (max - min)) + min;
 };
 
+const bedChange = document.querySelector(".bed")
+const empty = document.querySelector(".emptyBed")
+const wakeUpBtn = document.querySelector(".wakeUp")
+const miloMsg = document.querySelector(".helpMilo")
+const confirms = document.querySelector(".sure")
+const deny = document.querySelector(".no")
+const check = document.querySelector(".positive")
+const uncheck = document.querySelector(".negative")
+const launchMe = document.querySelector(".pickFit")
+const tfp = document.querySelector(".thanks")
+const again = document.querySelector(".again")
+
 const timeline = gsap.timeline({defaults:{duration:1}});
 timeline
-    .fromTo(".rocket", {scale: 0,rotation: 0},{rotation: 0, scale: 1, y: "-300%", x:"-50%", ease:"steps(6)"})
-    .fromTo(".rocket", {scale: 0, rotation: 120},{rotation: 160, delay: 2, scale: 1, y: "100%", x:"120%", ease:"steps(8)"})
+    .fromTo(".rocket", {scale: 0,rotation: 0},{rotation: 0, scale: 1, y: "-700%", x:"-50%", ease:"steps(6)"})
+    .fromTo(".rocket", {scale: 0, rotation: 120},{rotation: 160, delay: 2, scale: 1, y: "200%", x:"120%", ease:"steps(16)"})
     .fromTo(".rocket", {scale: 0,rotation: 0},{ rotation: 0, delay: 1, scale: 1, y: "-70%", x:"60%", ease:"steps(4)"})
     .from(".title", {y: "50%", opacity: "0", ease: "steps(8)"})
     .fromTo(".begin", {scale: 0, opacity: 0, y: "0%"},{delay: 1, scale: 1, opacity: 1, y: "100%", ease:"steps(4)"})
@@ -38,6 +50,18 @@ timeline
     .fromTo(".negative", {opacity:0},{opacity:0})
     .fromTo(".pickFit", {opacity:0},{opacity:0})
     .fromTo(".thanks", {opacity:0},{opacity:0})
+    bedChange.style.visibility = "hidden"; //visible bed cannot be clicked while invisible
+    empty.style.visibility = "hidden"; //invisible bed cannot be clicked either
+    wakeUpBtn.style.visibility = "hidden"; //wake up milo is hidden
+    miloMsg.style.visibility = "hidden"; //milo's help message is hidden
+    confirms.style.visibility = "hidden"; //I will help milo button hidden
+    deny.style.visibility = "hidden"; //I won't help hidden too
+    check.style.visibility = "hidden"; //positive response hidden
+    uncheck.style.visibility = "hidden"; //negative response hidden
+    launchMe.style.visibility = "hidden"; //launch me button hidden
+    tfp.style.visibility = "hidden"; //thank you for playing hidden
+    again.style.visibility = "hidden"; //thank you for playing hidden
+
     //Includes these elements on page, but theyre invisible.
 
 const button = document.querySelector('.begin')
@@ -45,7 +69,7 @@ button.addEventListener('click', () => {
 timeline
     .timeScale(3)
     .reverse()
-})
+});
 
 const rocketSound = new Audio()
 rocketSound.src = "rocket.mp3"
@@ -55,6 +79,9 @@ yawn.src = "yawn.mp3"
 
 button.addEventListener("click", () =>{
 const spawnClothes = gsap.timeline({defaults:{duration:1}})
+bedChange.style.visibility = "visible"; //visible bed cannot be clicked while invisible
+empty.style.visibility = "visible";//visible bed cannot be clicked while invisible
+wakeUpBtn.style.visibility = "visible"; //wake up milo is hidden
 spawnClothes
     .fromTo(".floor", {opacity:0, y:"200%"},{delay:4, opacity:1,ease: "bounce", y:"0%"})
     .fromTo(".bed", {opacity:0},{opacity:1})
@@ -64,15 +91,21 @@ spawnClothes
 const bed = document.querySelector(".bed")
 bed.addEventListener('click', () => {
     const bedSelect = gsap.timeline({defaults:{duration:.5, ease:"steps(2)"}})
-bedSelect
+    bedSelect
     .fromTo(".bed", {opacity: 1},{opacity: 0})
     .fromTo(".emptyBed",{opacity: 0}, {opacity: 1})
     .fromTo(".head", {opacity: 0},{opacity: 1})
     .fromTo(".defaultClothes", {opacity: 0},{opacity: 1})
     .fromTo(".wakeUp", {opacity: 1},{opacity: 0})
+    bedChange.style.visibility = "hidden";
+    miloMsg.style.visibility = "visible"; //milo's help message is hidden
+    confirms.style.visibility = "visible"; //I will help milo button hidden
+    deny.style.visibility = "visible"; //I won't help hidden too
+    bedSelect
     .fromTo(".helpMilo", {opacity: 0},{delay: 1, opacity: 1})
     .fromTo(".sure", {opacity:0},{opacity:1})
     .fromTo(".no", {opacity:0},{opacity:1})
+
 })
 
 bed.addEventListener('click', () => {
@@ -93,6 +126,8 @@ yes.addEventListener("click", () => {
     // gsap.fromTo(".spaceSuit", {opacity:0},{delay:7, opacity:1})
     // gsap.fromTo(".saiyan", {opacity:0},{delay:7,opacity:1})
     // gsap.fromTo(".jedi", {opacity:0},{delay:7,opacity:1})
+    check.style.visibility = "visible"; //positive response hidden
+    launchMe.style.visibility = "visible"; //positive response hidden
 })
 
 
@@ -106,7 +141,8 @@ no.addEventListener("click", () => {
     nextPage.fromTo(".negative", {opacity:1},{delay: 4, opacity:0})
     nextPage.fromTo(".no", {opacity:1},{opacity:0})
     nextPage.fromTo(".pickFit", {opacity:0},{opacity:1})
-    
+    uncheck.style.visibility = "visible"; //positive response hidden
+    launchMe.style.visibility = "visible"; //positive response hidden
     // gsap.fromTo(".spaceSuit", {opacity:0},{delay:7, opacity:1})
     // gsap.fromTo(".saiyan", {opacity:0},{delay:7,opacity:1})
     // gsap.fromTo(".jedi", {opacity:0},{delay:7,opacity:1})
@@ -121,10 +157,33 @@ launch.addEventListener("click", () => {
     gsap.fromTo(".floor", {opacity:1, y:"0%"},{delay:4, opacity:1,ease: "bounce", y:"200%"})
     gsap.fromTo(".thanks", {opacity:0},{delay:4, opacity:1})
     gsap.fromTo(".pickFit", {opacity:1},{opacity:0})
+    bedChange.style.visibility = "hidden"; //visible bed cannot be clicked while invisible
+    empty.style.visibility = "hidden"; //invisible bed cannot be clicked either
+    wakeUpBtn.style.visibility = "hidden"; //wake up milo is hidden
+    miloMsg.style.visibility = "hidden"; //milo's help message is hidden
+    confirms.style.visibility = "hidden"; //I will help milo button hidden
+    deny.style.visibility = "hidden"; //I won't help hidden too
+    check.style.visibility = "hidden"; //positive response hidden
+    uncheck.style.visibility = "hidden"; //negative response hidden
+    launchMe.style.visibility = "hidden"; //launch me button hidden
+    tfp.style.visibility = "hidden"; //thank you for playing hidden
+    again.style.visibility = "hidden"; //thank you for playing hidden
 
 })
+
+const reload = document.querySelector(".again")
 launch.addEventListener("click", () => {
     rocketSound.play();
+    launchMe.style.visibility = "hidden"; //positive response hidden
+    tfp.style.visibility = "visible";
+    gsap.to(".thanks", {opacity: 0, delay:10})
+    again.style.visibility = "visible";
+    gsap.fromTo(".again", {opacity: 0}, { delay: 11, opacity: 1}) //thank you for playing hidden
+})
+
+reload.addEventListener("click", () => {
+    location.reload()
+    again.style.visibility = "hidden";
 })
 
 // const selected = document.querySelectorAll(".outfit");
